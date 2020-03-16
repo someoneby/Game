b04ebb73ab969e48d7843127ec03f38b1bda3b25
someoneby <www.lapin.yan@gmail.com>
2020-03-17 00:15:17 +0300

    delete Game/*

--- Game/Items/Equipment/Equipment.cpp
+++
@@ -1,7 +0,0 @@
-#include "Equipment.h"
-
-// float Equipment::getIlvl() {
-//     return m_ilvl;
-// }
-
-Equipment::Equipment(int s_id, std::string s_name) : Item(s_id, s_name) {};
--- Game/Utility/RandDouble/RandDouble.h
+++
@@ -1,8 +0,0 @@
-#ifndef GAME_RANDDOUBLE_H
-#define GAME_RANDDOUBLE_H
-
-
-double randDouble ();
-
-
-#endif //GAME_RANDDOUBLE_H
